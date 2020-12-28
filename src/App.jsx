7d766053b19c5152f002c5aa29c3fb85c78fa3ee
David Mendoza <d.mendoza99@unitec.edu>
2020-12-28 15:40:11 -0600

    i think is done to start branching

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,52 @@
-import { React } from 'react';
+import { React, useRef } from 'react';
 import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import { Navbar, NavbarToggler, NavbarText } from 'reactstrap';
 import { sidebarRoutes, appRoutes } from './routes';
 
-const App = () => (
-  <Router>
-    <div className="d-flex" id="wrapper">
-      <div className="bg-dark border-right" id="sidebar-wrapper">
-        <div className="sidebar-heading"> 💧 Waterbase</div>
-        <div className="list-group list-group-flush">
-          {sidebarRoutes.map((route) => (
-            <NavLink
-              to={route.to}
-              exact
-              activeClassName="bg-secondary"
-              className="list-group-item list-group-item-action"
-            >
-              <i className={route.icon} /> {` ${route.name}`}
-            </NavLink>
-          ))}
+const App = () => {
+  const sidebar = useRef();
+  return (
+    <Router>
+      <div className="d-flex" ref={sidebar} id="wrapper">
+        <div className="bg-dark border-right" id="sidebar-wrapper">
+          <div className="sidebar-heading"> 💧 Waterbase</div>
+          <div className="list-group list-group-flush">
+            {sidebarRoutes.map((route) => (
+              <NavLink
+                to={route.to}
+                exact
+                activeClassName="bg-secondary"
+                className="list-group-item list-group-item-action"
+              >
+                <i className={route.icon} /> {` ${route.name}`}
+              </NavLink>
+            ))}
+          </div>
+        </div>
+        <div id="page-content-wrapper">
+          <Navbar color="light" light expand="md" className="justify-content-lg-end">
+            <NavbarToggler
+              onClick={() => {
+                if (!sidebar.current.classList.contains('toggled')) {
+                  sidebar.current.classList.add('toggled');
+                } else {
+                  sidebar.current.classList.remove('toggled');
+                }
+              }}
+            />
+            <NavbarText>Simple Text</NavbarText>
+          </Navbar>
+          <Switch>
+            {appRoutes.map((route) => (
+              <Route exact={route.exact} path={route.path}>
+                {route.component}
+              </Route>
+            ))}
+          </Switch>
         </div>
       </div>
-      <div className="page-content-wrapper">
-        <Switch>
-          {appRoutes.map((route) => (
-            <Route exact={route.exact} path={route.path}>
-              {route.component}
-            </Route>
-          ))}
-        </Switch>
-      </div>
-    </div>
-  </Router>
-);
+    </Router>
+  );
+};
 
 export default App;
